65303140957312a00a850d0b99ebdd8b45019ef5
NGUYEN QUANG LINH <nguyenquanglinh@topengnet.com>
2024-05-18 16:17:19 +0700

    Initialize random data

--- SCP_GA.cpp
+++ SCP_GA.cpp
@@ -3,10 +3,10 @@
 #include <time.h>
 
 #define KICH_THUOC_QUAN_THE 20
-#define SO_THE_HE 100 
+#define SO_THE_HE 100
 #define TY_LE_DOT_BIEN 0.1
-#define SO_TAP_CON 10
-#define SO_PHAN_TU 10
+#define SO_TAP_CON 20
+#define SO_PHAN_TU 20
 
 // Định nghĩa một tập hợp
 typedef struct {
@@ -18,6 +18,37 @@ typedef struct {
     TapHop cac_tap_con[SO_TAP_CON];
     int tap_goc[SO_PHAN_TU];
 } BaiToanPhuTap;
+void khoi_tao_du_lieu(BaiToanPhuTap *bai_toan) {
+    // Khởi tạo bộ sinh số ngẫu nhiên
+	srand(time(NULL)); 
+    // Khởi tạo tập gốc
+    for (int i = 0; i < SO_PHAN_TU; ++i) {
+    	// Khởi tạo ngẫu nhiên 0 hoặc 1
+        bai_toan->tap_goc[i] = rand() % 2; 
+    }
+    // Khởi tạo các tập con
+    for (int i = 0; i < SO_TAP_CON; ++i) {
+        for (int j = 0; j < SO_PHAN_TU; ++j) {
+        	// Khởi tạo ngẫu nhiên 0 hoặc 1
+            bai_toan->cac_tap_con[i].cac_phan_tu[j] = rand() % 2; 
+        }
+    }
+}
+
+void in_du_lieu(BaiToanPhuTap *bai_toan) {
+    printf("Tap goc: ");
+    for (int i = 0; i < SO_PHAN_TU; ++i) {
+        printf("%d ", bai_toan->tap_goc[i]);
+    }
+    printf("\nCac tap con:\n");
+    for (int i = 0; i < SO_TAP_CON; ++i) {
+        printf("Tap con %d: ", i + 1);
+        for (int j = 0; j < SO_PHAN_TU; ++j) {
+            printf("%d ", bai_toan->cac_tap_con[i].cac_phan_tu[j]);
+        }
+        printf("\n");
+    }
+}
 
 // Hàm đánh giá - trả về số phần tử trong tập gốc mà được bao phủ bởi một tập con
 int danh_gia(BaiToanPhuTap *bai_toan, TapHop tap_hop) {
@@ -79,85 +110,64 @@ void in_quan_the(TapHop quan_the[], int the_he) {
     }
 }
 
-void khoi_tao_du_lieu(BaiToanPhuTap *bai_toan) {
-    srand(time(NULL)); // Khởi tạo bộ sinh số ngẫu nhiên
+int main() {
+    // Khởi tạo dữ liệu
+    BaiToanPhuTap bai_toan;
+	khoi_tao_du_lieu(&bai_toan);
 
-    // Khởi tạo tập gốc
-    for (int i = 0; i < SO_PHAN_TU; ++i) {
-        bai_toan->tap_goc[i] = rand() % 2; // Khởi tạo ngẫu nhiên 0 hoặc 1
-    }
-
-    // Khởi tạo các tập con
-    for (int i = 0; i < SO_TAP_CON; ++i) {
-        for (int j = 0; j < SO_PHAN_TU; ++j) {
-            bai_toan->cac_tap_con[i].cac_phan_tu[j] = rand() % 2; // Khởi tạo ngẫu nhiên 0 hoặc 1
+    // Khởi tạo quần thể
+    TapHop quan_the[KICH_THUOC_QUAN_THE];
+    int i, j;
+    for (i = 0; i < KICH_THUOC_QUAN_THE; ++i) {
+        for (j = 0; j < SO_TAP_CON; ++j) {
+            quan_the[i].cac_phan_tu[j] = rand() % 2; // Khởi tạo ngẫu nhiên
         }
     }
-}
 
-    int main() {
+    // Vòng lặp thuật toán di truyền
+    for (i = 0; i < SO_THE_HE; ++i) {
+        // Đánh giá tất cả cá thể
+        int quan_the_danh_gia[KICH_THUOC_QUAN_THE];
+        for (j = 0; j < KICH_THUOC_QUAN_THE; ++j) {
+            quan_the_danh_gia[j] = phu_hop(&bai_toan, quan_the[j]);
+        }
         
-        // Khởi tạo dữ liệu
-        BaiToanPhuTap bai_toan;
-    	khoi_tao_du_lieu(&bai_toan);
+        // In quần thể hiện tại
+    	in_quan_the(quan_the, i);
 
-        // Khởi tạo quần thể
-        TapHop quan_the[KICH_THUOC_QUAN_THE];
-        int i, j;
-        for (i = 0; i < KICH_THUOC_QUAN_THE; ++i) {
-            for (j = 0; j < SO_TAP_CON; ++j) {
-                quan_the[i].cac_phan_tu[j] = rand() % 2; // Khởi tạo ngẫu nhiên
-            }
+        // Chọn lọc cá thể cho lai ghép
+        int tong_phu_hop = 0;
+        for (j = 0; j < KICH_THUOC_QUAN_THE; ++j) {
+            tong_phu_hop += quan_the_danh_gia[j];
         }
 
-        // Vòng lặp thuật toán di truyền
-        for (i = 0; i < SO_THE_HE; ++i) {
-            // Đánh giá tất cả cá thể
-            int quan_the_danh_gia[KICH_THUOC_QUAN_THE];
-            for (j = 0; j < KICH_THUOC_QUAN_THE; ++j) {
-                quan_the_danh_gia[j] = phu_hop(&bai_toan, quan_the[j]);
-            }
-            
-            // In quần thể hiện tại
-        	in_quan_the(quan_the, i);
-
-            // Chọn lọc cá thể cho lai ghép
-            int tong_phu_hop = 0;
-            for (j = 0; j < KICH_THUOC_QUAN_THE; ++j) {
-                tong_phu_hop += quan_the_danh_gia[j];
-            }
-
-            TapHop quan_the_moi[KICH_THUOC_QUAN_THE];
-            for (j = 0; j < KICH_THUOC_QUAN_THE; ++j) {
-                // Lựa chọn bằng cách quay xe
-                int phu_hop_duoc_chon = rand() % tong_phu_hop;
-                int tong_phu_hop_tam = 0;
-                int k;
-                for (k = 0; k < KICH_THUOC_QUAN_THE; ++k) {
-                    tong_phu_hop_tam += quan_the_danh_gia[k];
-                    if (tong_phu_hop_tam >= phu_hop_duoc_chon) {
-                        quan_the_moi[j] = quan_the[k];
-                        break;
-                    }
+        TapHop quan_the_moi[KICH_THUOC_QUAN_THE];
+        for (j = 0; j < KICH_THUOC_QUAN_THE; ++j) {
+            // Lựa chọn bằng cách quay xe
+            int phu_hop_duoc_chon = rand() % tong_phu_hop;
+            int tong_phu_hop_tam = 0;
+            int k;
+            for (k = 0; k < KICH_THUOC_QUAN_THE; ++k) {
+                tong_phu_hop_tam += quan_the_danh_gia[k];
+                if (tong_phu_hop_tam >= phu_hop_duoc_chon) {
+                    quan_the_moi[j] = quan_the[k];
+                    break;
                 }
             }
-
-            // Lai ghép và đột biến
-            for (j = 0; j < KICH_THUOC_QUAN_THE; ++j) {
-                int cha1_index = rand() % KICH_THUOC_QUAN_THE;
-                int cha2_index = rand() % KICH_THUOC_QUAN_THE;
-                TapHop con = lai_ghep(quan_the_moi[cha1_index], quan_the_moi[cha2_index]);
-                dot_bien(&con, TY_LE_DOT_BIEN);
-                quan_the[j] = con;
-            }
-        }
-
-        // In ra giải pháp
-        printf("Giai phap: ");
-        for (i = 0; i < SO_TAP_CON; ++i) {
-            printf("%d ", quan_the[0].cac_phan_tu[i]);
-        }
-        printf("\n");
-
-        return 0;
+        } 
+        
+        
     }
+    
+	// In tập gốc và các tập con
+    in_du_lieu(&bai_toan);
+    
+    // In ra giải pháp
+    printf("\nGIAI PHAP: ");
+    for (i = 0; i < SO_TAP_CON; ++i) {
+        printf("%d ", quan_the[0].cac_phan_tu[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
